77c6f39d9db04c732fe8e820c2b4240e1d6a9b2c
Samantha Kerr <samantha-kerr@hotmail.ca>
2019-10-01 17:31:39 -0400

    #3 Base of Player.h and Player.cpp files, contains Player, Army, and City class with defined instance members

---
+++ EightMinuteEmpire/Player.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <vector> 
+#include <list>
+#include <string>
+#include <iostream>
+#include "Map.h"
+
+using namespace std;
+
+class Army;
+class City;
+class Player;
+
+class Army
+{
+public:
+	Army(Player*, int);
+	~Army();
+
+	int* id;
+	Player* player;
+	Country* occupiedCountry;
+
+	void setOccupiedCountry(Country*);
+};
+
+class City
+{
+public:
+	City(Player*, int);
+	~City();
+
+	int* id;
+	Player* player;
+	Country* occupiedCountry;
+
+	void setOccupiedCountry(Country*);
+};
+
+class Player
+{
+public:
+	Player(string, int, string);
+	~Player();
+
+	string* name;
+	int* numCoins;
+	string* color;
+	vector<Army*>* armies;
+	vector<City*>* cities;
+	vector<Country*>* ownedCountries;	// Part 1
+	// vector<Card*>* hand;				// Part 4
+	// vector<Good*>* goods				// Part 4
+	//BidingFacility* bidingFacility;	// Part 5
+
+	void createArmies();
+	void createCities();
+
+	void PayCoin(int);
+	void PlaceNewArmies();
+	void MoveArmies();
+	void MoveOverLand();
+	void BuildCity(Country*);
+	void DestroyArmy(Country*);
+};
