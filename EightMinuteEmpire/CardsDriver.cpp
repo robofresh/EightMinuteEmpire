a90c142b028796e5d8d132c6bccfa056b1c6538a
Samantha Kerr <samantha-kerr@hotmail.ca>
2019-10-12 17:00:07 -0400

    Uncommented drivers

--- EightMinuteEmpire/CardsDriver.cpp
+++ EightMinuteEmpire/CardsDriver.cpp
@@ -5,34 +5,33 @@ int main()
 {
     Deck* deck = new Deck(); //make a deck object
 	//Testing purpose
-	//Cards* cards0 = new Cards("0", "invade");
-	//Cards* cards1 = new Cards("1", "invade");
-	//Cards* cards2 = new Cards("2", "invade");
-	//Cards* cards3 = new Cards("3", "invade"); 
-	//Cards* cards4 = new Cards("4", "invade"); 
-	//Cards* cards5 = new Cards("5", "invade"); 
-	//Cards* cards6 = new Cards("6", "invade");
+	Cards* cards0 = new Cards("0", "invade");
+	Cards* cards1 = new Cards("1", "invade");
+	Cards* cards2 = new Cards("2", "invade");
+	Cards* cards3 = new Cards("3", "invade"); 
+	Cards* cards4 = new Cards("4", "invade"); 
+	Cards* cards5 = new Cards("5", "invade"); 
+	Cards* cards6 = new Cards("6", "invade");
 
-	//deck->stackofCards->push(cards0);
-	//deck->stackofCards->push(cards1);
-	//deck->stackofCards->push(cards2);
-	//deck->stackofCards->push(cards3);
-	//deck->stackofCards->push(cards4);
-	//deck->stackofCards->push(cards5);
-	//deck->stackofCards->push(cards6);
+	deck->stackofCards->push(cards0);
+	deck->stackofCards->push(cards1);
+	deck->stackofCards->push(cards2);
+	deck->stackofCards->push(cards3);
+	deck->stackofCards->push(cards4);
+	deck->stackofCards->push(cards5);
+	deck->stackofCards->push(cards6);
 
-	//deck->initialDraw();
+	deck->initialDraw();
 
-	//for (int i = 0; i < deck->drawingcards->faceupcards->size(); i++)
-	//{
-	//	cout << *deck->drawingcards->faceupcards->at(i)->good << endl;
-	//}
+	for (int i = 0; i < deck->drawingcards->faceupcards->size(); i++)
+	{
+		cout << *deck->drawingcards->faceupcards->at(i)->good << endl;
+	}
 
-	//deck->drawingcards->exchange(3);
+	deck->drawingcards->exchange(3);
 
-	//for (int i = 0; i < deck->drawingcards->faceupcards->size(); i++)
-	//{
-	//	cout << *deck->drawingcards->faceupcards->at(i)->good << endl;
-	//}
-	
+	for (int i = 0; i < deck->drawingcards->faceupcards->size(); i++)
+	{
+		cout << *deck->drawingcards->faceupcards->at(i)->good << endl;
+	}
 }
--- EightMinuteEmpire/MapDriver.cpp
+++ EightMinuteEmpire/MapDriver.cpp
@@ -1,71 +1,70 @@
-//#include <iostream>
-//#include "Map.h"
+#include <iostream>
+#include "Map.h"
+
+
+//still dont know what the assignment means by subgraph
+//dont know what "testing for valid/invalid maps" means, surely that goes in the map loader
+
+int main()
+{
+	//input will take all countries, continents, adjacecnt countries etc.
+	
+	//will make a vector of country pointers and have that pointed to by map
+	//will make a vector of countries' adjacent countries and have each country point to their own
+	//will make a vector of countries in each continent and point to that in each contitnent
+
+
+	Map *map = new Map(); // make a map object
+
+	//simulate input...
+	//this will likely change and depending how a file is formated, the contient may add it's countries when created as a nested for loop
+
+	for (int i = 0; i < 10; i++)
+	{
+		Continent* cont = map->createContinent("Contitnent " + to_string(i / 2));
+		Country* country = map->createCountry("Country " + to_string(i) , cont);
+
+		if (country != nullptr)
+		{
+			for (int j = 0; j < i; j += 2)
+			{
+				country->adjCountries->push_back(map->mapCountries->at(j));
+			}
+		}
+		
+	}
+
+	for (int i = 0; i < map->mapCountries->size(); i++)
+	{
+		cout << map->mapCountries->at(i)->name->c_str();
+		for (int j = 0; j < map->mapCountries->at(i)->adjCountries->size(); j++)
+		{
+			cout << " -> " << map->mapCountries->at(i)->adjCountries->at(j)->name->c_str();
+		}
+		cout << endl;
+	}
+
+
+	for (int i = 0; i < map->mapContinents->size(); i++)
+	{
+		cout << map->mapContinents->at(i)->name->c_str() << endl;
+	}
+
+
+	//cout << map->mapCountries->at(0)->name->c_str() << endl; //print string from a coutry via map
+
+
+}
 //
 //
-////still dont know what the assignment means by subgraph
-////dont know what "testing for valid/invalid maps" means, surely that goes in the map loader
 //
-//int main()
-//{
-//	//input will take all countries, continents, adjacecnt countries etc.
-//	
-//	//will make a vector of country pointers and have that pointed to by map
-//	//will make a vector of countries' adjacent countries and have each country point to their own
-//	//will make a vector of countries in each continent and point to that in each contitnent
+//Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
 //
-//
-//	Map *map = new Map(); // make a map object
-//
-//
-//	//simulate input...
-//	//this will likely change and depending how a file is formated, the contient may add it's countries when created as a nested for loop
-//
-//	for (int i = 0; i < 10; i++)
-//	{
-//		Continent* cont = map->createContinent("Contitnent " + to_string(i / 2));
-//		Country* country = map->createCountry("Country " + to_string(i) , cont);
-//
-//		if (country != nullptr)
-//		{
-//			for (int j = 0; j < i; j += 2)
-//			{
-//				country->adjCountries->push_back(map->mapCountries->at(j));
-//			}
-//		}
-//		
-//	}
-//
-//	for (int i = 0; i < map->mapCountries->size(); i++)
-//	{
-//		cout << map->mapCountries->at(i)->name->c_str();
-//		for (int j = 0; j < map->mapCountries->at(i)->adjCountries->size(); j++)
-//		{
-//			cout << " -> " << map->mapCountries->at(i)->adjCountries->at(j)->name->c_str();
-//		}
-//		cout << endl;
-//	}
-//
-//
-//	for (int i = 0; i < map->mapContinents->size(); i++)
-//	{
-//		cout << map->mapContinents->at(i)->name->c_str() << endl;
-//	}
-//
-//
-//	//cout << map->mapCountries->at(0)->name->c_str() << endl; //print string from a coutry via map
-//
-//
-//}
-////
-////
-////
-////Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-//// Debug program: F5 or Debug > Start Debugging menu
-////
-//// Tips for Getting Started: 
-////   1. Use the Solution Explorer window to add/manage files
-////   2. Use the Team Explorer window to connect to source control
-////   3. Use the Output window to see build output and other messages
-////   4. Use the Error List window to view errors
-////   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-////   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
